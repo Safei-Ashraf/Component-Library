5c0ff1da0391acd1214ff91bf7e9f76ad91d882f
asif mehmood <amehmood999@gmail.com>
2022-01-31 23:22:22 +0500

    cleanup: remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline text-center  ">Hello world!</h1>
   );
 };
