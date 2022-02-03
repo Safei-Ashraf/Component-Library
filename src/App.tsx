fae56a74671573caaafef9704a8773ee91bdad39
Safei Ashraf <safei.ashraf@gmail.com>
2022-02-03 21:00:10 +0200

    add eslint configs, to use AirBnB style guide, test it in some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline text-center  ">Hello world!</h1>
-  );
+	return (
+		<h1 className="text-3xl font-bold underline text-center  ">
+			Hello world!
+		</h1>
+	);
 };
