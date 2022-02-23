9321c61e7d460ff25b4d758b9a6d9da8fdcee50d
Safei Ashraf <safei.ashraf@gmail.com>
2022-02-23 14:35:31 +0200

    fix: resolving component type issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  content: [path.join(__dirname, "./src/**/*.(js|jsx|ts|tsx)")],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
