aba50fbb2d2a1efe25b6b09acbaf4c7114bb0341
Safei Ashraf <safei.ashraf@gmail.com>
2022-04-02 15:38:28 +0200

    fix: cleanup app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable react/self-closing-comp */
-import Button from "./stories/components/Button/Button";
-
 export function App() {
   return (
     <div>
       <>
-        <div>what</div>
-        <Button type="primary"></Button>
+        <div>app</div>
       </>
     </div>
   );
